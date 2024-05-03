1a41c6f9781e6cd4d394a29393c8e582f90190f4
jhl221123 <jhl221123@naver.com>
2024-05-03 17:52:43 +0900

    [#4] test: 회원 CRUD 테스트

---
+++ src/test/java/enjoytrip/member/mock/MockMember.java
@@ -0,0 +1,22 @@
+package enjoytrip.member.mock;
+
+import enjoytrip.member.domain.Gender;
+import enjoytrip.member.domain.Member;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MockMember extends Member {
+    private boolean isUpdated = false;
+
+    public MockMember(Long id, String email, String password, String name, Integer age, Gender gender, String phoneNumber, LocalDateTime createAt, LocalDateTime updatedAt, String createdBy, String updatedBy) {
+        super(id, email, password, name, age, gender, phoneNumber, createAt, updatedAt, createdBy, updatedBy);
+    }
+
+    @Override
+    public void update(String email, String password, String name, Integer age, Gender gender, String phoneNumber, LocalDateTime updatedAt, String updatedBy) {
+        super.update(email, password, name, age, gender, phoneNumber, updatedAt, updatedBy);
+        isUpdated = true;
+    }
+}
