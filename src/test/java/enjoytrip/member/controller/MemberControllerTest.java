5f6d7282a6885ff6fbbe02a5164d67dc04d4580e
jhl221123 <jhl221123@naver.com>
2024-05-16 16:28:57 +0900

    [#17] test: 비밀번호 변경 기능 테스트

--- src/test/java/enjoytrip/member/controller/MemberControllerTest.java
+++ src/test/java/enjoytrip/member/controller/MemberControllerTest.java
@@ -1,15 +1,23 @@
 package enjoytrip.member.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import enjoytrip.member.domain.Gender;
 import enjoytrip.member.domain.Member;
+import enjoytrip.member.dto.request.MemberPasswordUpdateRequest;
 import enjoytrip.member.dto.request.MemberSaveRequest;
 import enjoytrip.member.dto.request.MemberUpdateRequest;
 import enjoytrip.member.dto.response.MemberFindResponse;
 import enjoytrip.member.dto.response.MemberSaveResponse;
 import enjoytrip.member.dto.response.MemberUpdateResponse;
 import enjoytrip.member.service.MemberService;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,13 +29,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.time.LocalDateTime;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberControllerTest {
@@ -87,17 +88,37 @@ class MemberControllerTest {
 
   @Test
   @DisplayName("회원 정보 수정 요청 성공")
-  void update() throws Exception {
+  void updateInfo() throws Exception {
     //given
     MemberUpdateRequest request = getMemberUpdateRequest();
     MemberUpdateResponse response = new MemberUpdateResponse(1L);
     String requestJson = objectMapper.registerModule(new JavaTimeModule())
         .writeValueAsString(request);
     String responseJson = objectMapper.writeValueAsString(response);
-    doReturn(response).when(memberService).update(any(MemberUpdateRequest.class));
+    doReturn(response).when(memberService).updateInfo(any(MemberUpdateRequest.class));
 
     //expected
     mockMvc.perform(MockMvcRequestBuilders.put("/members/{id}", 1L)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestJson)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().json(responseJson))
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("회원 비밀번호 수정 요청 성공")
+  void updatePassword() throws Exception {
+    //given
+    MemberPasswordUpdateRequest request = getMemberPasswordUpdateRequest();
+    MemberUpdateResponse response = new MemberUpdateResponse(1L);
+    String requestJson = objectMapper.writeValueAsString(request);
+    String responseJson = objectMapper.writeValueAsString(response);
+    doReturn(response).when(memberService).updatePassword(any(MemberPasswordUpdateRequest.class));
+
+    //expected
+    mockMvc.perform(MockMvcRequestBuilders.put("/members/{id}/password", 1L)
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestJson)
         )
@@ -120,13 +141,20 @@ class MemberControllerTest {
   private static MemberUpdateRequest getMemberUpdateRequest() {
     return MemberUpdateRequest.builder()
         .id(1L)
-        .email("test@email.com")
-        .password("test")
-        .name("Test")
+        .name("name")
         .age(20)
         .gender(Gender.FEMALE)
         .phoneNumber("010-1234-5678")
-        .updatedBy("Test")
+        .updatedBy("name")
+        .build();
+  }
+
+  private static MemberPasswordUpdateRequest getMemberPasswordUpdateRequest() {
+    return MemberPasswordUpdateRequest.builder()
+        .id(1L)
+        .currentPassword("password")
+        .newPassword("newPassword")
+        .checkPassword("newPassword")
         .build();
   }
 
@@ -137,13 +165,13 @@ class MemberControllerTest {
   private static MemberSaveRequest getMemberSaveRequest() {
     return MemberSaveRequest.builder()
         .email("test@email.com")
-        .password("test")
-        .name("Test")
+        .password("password")
+        .name("name")
         .age(20)
         .gender(Gender.FEMALE)
         .phoneNumber("010-1234-5678")
-        .createdBy("Test")
-        .updatedBy("Test")
+        .createdBy("name")
+        .updatedBy("name")
         .build();
   }
 
@@ -151,15 +179,15 @@ class MemberControllerTest {
     return Member.builder()
         .id(1L)
         .email("test@email.com")
-        .password("test")
-        .name("Test")
+        .password("password")
+        .name("name")
         .age(20)
         .gender(Gender.FEMALE)
         .phoneNumber("010-1234-5678")
         .createAt(LocalDateTime.now())
-        .createdBy("Test")
+        .createdBy("name")
         .updatedAt(LocalDateTime.now())
-        .updatedBy("Test")
+        .updatedBy("name")
         .build();
   }
 }
