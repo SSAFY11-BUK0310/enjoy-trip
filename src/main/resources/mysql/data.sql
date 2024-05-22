5191456c6eac89a8acdc0308e960a842167e53d9
jhl221123 <jhl221123@naver.com>
2024-05-22 12:06:08 +0900

    [#28] chore: 샘플 데이터 추가

--- src/main/resources/mysql/data.sql
+++ src/main/resources/mysql/data.sql
@@ -31,16 +31,24 @@ insert into image values(8, 3, 'image_name8', 'image_uuid8', 'directory_uuid8', 
 insert into image values(9, 4, 'image_name9', 'image_uuid9', 'directory_uuid9', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
 
 -- comment
-insert into comment values(1, 1, 1, null, 'content1', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
+insert into comment values(1, 1, 1, 1, 'content1', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
 insert into comment values(2, 2, 1, 1, 'content2', date(now()), date(now()), 'test2@email.com', 'test2@email.com');
 insert into comment values(3, 3, 1, 1, 'content3', date(now()), date(now()), 'test3@email.com', 'test3@email.com');
 insert into comment values(4, 4, 1, 1, 'content4', date(now()), date(now()), 'test4@email.com', 'test4@email.com');
-insert into comment values(5, 2, 2, null, 'content5', date(now()), date(now()), 'test2@email.com', 'test2@email.com');
+insert into comment values(5, 2, 2, 5, 'content5', date(now()), date(now()), 'test2@email.com', 'test2@email.com');
 insert into comment values(6, 3, 2, 5, 'content6', date(now()), date(now()), 'test3@email.com', 'test3@email.com');
 insert into comment values(7, 4, 2, 5, 'content7', date(now()), date(now()), 'test4@email.com', 'test4@email.com');
-insert into comment values(8, 5, 2, null, 'content8', date(now()), date(now()), 'test5@email.com', 'test5@email.com');
+insert into comment values(8, 5, 2, 8, 'content8', date(now()), date(now()), 'test5@email.com', 'test5@email.com');
 insert into comment values(9, 6, 2, 8, 'content9', date(now()), date(now()), 'test6@email.com', 'test6@email.com');
-insert into comment values(10, 7, 3, null, 'content10', date(now()), date(now()), 'test7@email.com', 'test7@email.com');
+insert into comment values(10, 7, 3, 10, 'content10', date(now()), date(now()), 'test7@email.com', 'test7@email.com');
+insert into comment values(11, 1, 10, 11, '글 잘 봤습니다! 혹시 관련 자료나 추천 서적이 있다면 공유해 주실 수 있을까요?', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
+insert into comment values(12, 2, 10, 11, '무라카미 하루키의 "먼 북소리" 추천드려요!', date(now()), date(now()), 'test2@email.com', 'test2@email.com');
+insert into comment values(13, 2, 10, 11, '여행하면서 읽기 좋더라구요 :)', date(now()), date(now()), 'test2@email.com', 'test2@email.com');
+insert into comment values(14, 1, 10, 11, '추천 감사합니다 ㅎㅎ', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
+insert into comment values(15, 1, 10, 15, '항상 좋은 글 감사합니다. 이번 글도 많은 도움이 되었습니다. 다음 포스트도 기대할게요!', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
+insert into comment values(16, 3, 10, 15, '안녕하세요, 질문이 있어서 댓글 남깁니다. 이 글과 관련된 주제에 대해 좀 더 자세히 알고 싶습니다.', date(now()), date(now()), 'test3@email.com', 'test3@email.com');
+insert into comment values(17, 1, 10, 15, '안녕하세요! 관심 가져주셔서 감사합니다. 어떤 부분이 궁금하신가요?', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
+insert into comment values(18, 1, 10, 18, '구체적으로 질문해 주시면 최대한 자세히 답변드리겠습니다.', date(now()), date(now()), 'test1@email.com', 'test1@email.com');
 
 -- message_room
 insert into message_room values(1, 1, 1, 2, date(now()), date(now()), 'test1@email.com', 'test2@email.com');
